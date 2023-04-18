8b1ccd0fcfc6ebcd68804d39e815d9ad1208ff17
pirate <17961436+pirateunclejack@users.noreply.github.com>
2023-04-18 21:25:27 +0800

    Add teacher table

--- db/db.sql
+++ db/db.sql
@@ -1,4 +1,5 @@
 drop table if exists course;
+drop table if exists teacher;
 
 create table course
 (
@@ -15,9 +16,26 @@ create table course
     level varchar(30)
 );
 
+create table teacher
+(
+    id serial primary key,
+    name varchar(140) not null,
+    picture_url varchar(100),
+    profile varchar(2000)
+);
+
 insert into course 
     (id, teacher_id, name, time)
 values(1, 1, 'First course', '2022-01-17 05:40:00');
+
 insert into course 
     (id, teacher_id, name, time)
 values(2, 1, 'Second course', '2022-01-18 05:45:00');
+
+insert into teacher 
+    (id, name, picture_url, profile)
+values(1, 'nihao', 'https:://www.baidu.com', 'best teacher');
+
+insert into teacher 
+    (id, name, picture_url, profile)
+values(2, 'wohao', 'https:://www.google.com', 'best teacher 2');
